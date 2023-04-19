76aae4552ac7ebe432362604d63aee1694db421d
GaiaFilippa <gaiafilippa2001@gmail.com>
2023-04-19 14:44:11 +0200

    set up classe prodotto e classe negozio

--- CSharp Composizione Shop/Negozio.cs
+++ CSharp Composizione Shop/Negozio.cs
@@ -8,5 +8,58 @@ namespace CSharp_Composizione_Shop
 {
     public class Negozio
     {
+        // ATTRIBUTI
+        private string nomeNegozio;
+        private string citta;
+        private string indirizzo;
+        private int numeroCivico;
+        private List<Prodotto> prodotti;
+
+
+
+        // COSTRUTTORE
+        public Negozio(string nomeNegozio, string citta, string indirizzo, int numeroCivico)
+        {
+            this.nomeNegozio = nomeNegozio;
+            this.citta = citta;
+            this.indirizzo = indirizzo;
+            this.numeroCivico = numeroCivico;
+
+            this.prodotti = new List<Prodotto>();
+
+        }
+
+
+        // GETTERS
+        public string GetNomeNegozio()
+        {
+            return this.nomeNegozio;
+        }
+
+        public string GetCitta()
+        {
+            return this.citta;
+        }
+
+        public string GetIndirizzo()
+        {
+            return this.indirizzo;
+        }
+        public int GetNumeroCivico()
+        {
+            return this.numeroCivico;
+        }
+
+        public List<Prodotto> GetListaProdotti()
+        {
+            return this.prodotti;
+        }
+
+        // SETTERS
+
+
+
+        // METODI
+
     }
 }
--- CSharp Composizione Shop/Prodotto.cs
+++ CSharp Composizione Shop/Prodotto.cs
@@ -8,5 +8,61 @@ namespace CSharp_Composizione_Shop
 {
     public class Prodotto
     {
+        // ATTRIBUTI
+        private int codice;
+        private string nome;
+        private string descrizione;
+        private float prezzo;
+
+
+        // STATI
+
+
+        // COSTRUTTORE
+        public Prodotto(string nome, string descrizione, float prezzo)
+        {
+            this.nome = nome;
+            this.descrizione = descrizione;
+            this.prezzo = prezzo;
+
+            Random codiceRandom = new Random();
+            int codice = codiceRandom.Next();
+            this.codice = codice;
+
+        }
+
+        // GETTERS
+
+        public int GetCodice()
+        {
+            return codice;
+        }
+
+        public string GetDescrizione()
+        {
+            return this.descrizione;
+        }
+
+        public string GetNome()
+        {
+            return this.nome;
+        }
+
+        // SETTERS
+
+        public float SetPrezzo()
+        {
+            return (float)this.prezzo;
+        }
+
+        // METODI
+
+        public string ConcatenaNomeECodice()
+        {
+            string nomeECodice = nome + this.codice;
+            return nomeECodice;
+        }
+
+
     }
 }
